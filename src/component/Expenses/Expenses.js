afce3c8c4e5fae04d3e7ac337fd66e7920529166
조영찬 <whdudcks98@naver.com>
2023-03-06 17:40:15 +0900

    2023-03-06 리액트 공부

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css'
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-  return (
-    <Card className='expenses'>
-        <ExpenseItem 
-            title={props.expenses[0].title} 
-            amount={props.expenses[0].amount} 
-            date={props.expenses[0].date}>
-        </ExpenseItem>
-        <ExpenseItem 
-            title={props.expenses[1].title} 
-            amount={props.expenses[1].amount} 
-            date={props.expenses[1].date}>
-        </ExpenseItem>
-        <ExpenseItem 
-            title={props.expenses[2].title} 
-            amount={props.expenses[2].amount} 
-            date={props.expenses[2].date}>
-        </ExpenseItem>
-        <ExpenseItem 
-            title={props.expenses[3].title} 
-            amount={props.expenses[3].amount} 
-            date={props.expenses[3].date}>
-        </ExpenseItem>
-    </Card>
-  )
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
+    const filteredExpenses = props.items.filter((item) => {
+        return item.date.getFullYear().toString() === filteredYear;
+    })
+
+    return (
+        <div>
+            <Card className='expenses'>
+                <ExpenseFilter 
+                    selected={filteredYear} 
+                    onChangeFilter={filterChangeHandler} 
+                />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </div>
+    )
 }
 
 export default Expenses
